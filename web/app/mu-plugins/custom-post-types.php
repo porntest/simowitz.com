3b10d3d70d140407471c0c96426799fc48f49ccb
Dylan Simowitz <dsimowitz@gmail.com>
2016-12-12 21:48:16 -0800

    Add custom post types plugin.

---
+++ web/app/mu-plugins/custom-post-types.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name:  Custom Post Types 
+Plugin URI:   https://dylan.simowitz.com/
+Description:  Custom post types for simowitz.com.
+Version:      1.0.0
+Author:       Dylan Simowitz
+Author URI:   https://dylan.simowitz.com/
+License:      MIT License
+*/
+
+use PostTypes\PostType
+
+new PostType('case')
