cf22a6064698209dc881076acef75651950ddb70
Dylan Simowitz <dsimowitz@gmail.com>
2016-12-12 18:44:05 -0800

    Add a home page template

---
+++ web/app/themes/simowitz/templates/page-home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.base')
+
+@section('content')
+  @include('partials.page-header')
+    <div class="row">
+      <div class="col-xs">
+        <h3>header 1</h3>
+      </div>
+      <div class="col-xs">
+        <h3>header 2</h3> 
+      </div>
+      <div class="col-xs">
+        <h3>header 3</h3> 
+      </div>
+    </div>
+@endsection
