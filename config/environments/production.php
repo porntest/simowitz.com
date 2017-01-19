94da216c52be22d84174abb61a3d9291f0cb8f1e
Dylan Simowitz <dsimowitz@gmail.com>
2017-01-18 18:16:27 -0800

    Fix SSL redierct loop

--- config/environments/production.php
+++ config/environments/production.php
@@ -6,3 +6,5 @@ define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
 define('FORCE_SSL_ADMIN', true);
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
+       $_SERVER['HTTPS']='on';
